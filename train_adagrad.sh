6a3091bd630f3fb3846a2d8db6a216e83f24d489
gnanukoth <gnanukoth@gmail.com>
2021-10-22 14:01:36 +0800

    Added train_adagrad.py and train_adagrad.sh file

---
+++ train_adagrad.sh
@@ -0,0 +1,16 @@
+NAME=fake-news-classifier-Adagrad-Optimizer-v0
+
+rm ../checkpoint/$NAME/train_log.txt
+
+CUDA_VISIBLE_DEVICES=0 python train_adagrad.py --outdir checkpoint/$NAME \
+--train_path data/coaid/FNSC_mapped_users_train.json \
+--valid_path data/coaid/FNSC_mapped_users_test.json \
+--epoch 5
+--learning_rate 1e-2
+
+CUDA_VISIBLE_DEVICES=0 python test.py --outdir ./output/ \
+--test_path data/coaid/FNSC_mapped_users_test.json \
+--checkpoint checkpoint/$NAME/model.best.pt \
+--name $NAME-dev.json
+
+python calculate_results.py --input output/$NAME-dev.json --ground_trut data/coaid/FNSC_mapped_users_test.json
